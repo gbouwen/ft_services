c565a5cd167f279484180b2ceb7e4ef8313332ae
gbouwen <gijsbouwen@gmail.com>
2020-08-18 10:33:00 +0200

    started on linux

--- setup.sh
+++ setup.sh
@@ -11,3 +11,23 @@
 # **************************************************************************** #
 
 minikube start --driver=virtualbox
+
+# Create system secrets
+kubectl create -f srcs/yaml_files/secrets.yaml
+
+# MetalLB
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/namespace.yaml
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.3/manifests/metallb.yaml
+# On first install only
+kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+
+kubectl create -f srcs/yaml_files/metallb.yaml
+
+# Nginx
+kubectl create -f srcs/yaml_files/nginx.yaml
+
+# MySQL
+kubectl create -f srcs/yaml_files/mysql.yaml
+
+# PhpMyAdmin
+kubectl create -f srcs/yaml_files/phpmyadmin.yaml
