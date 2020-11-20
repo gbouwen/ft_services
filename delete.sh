be5678151d3b3eda41ddd22f04da34233f0fc227
gbouwen <gijsbouwen@gmail.com>
2020-11-20 18:14:31 +0100

    fuck pma

--- delete.sh
+++ delete.sh
@@ -1,3 +1,5 @@
 kubectl delete deployment mysql
 kubectl delete svc mysql
 kubectl delete pvc mysql-pv-claim
+kubectl delete deployment phpmyadmin
+kubectl delete svc phpmyadmin
--- remove.sh
+++ remove.sh
@@ -1,2 +1,3 @@
 eval $(minikube docker-env)
 docker rmi mysql
+docker rmi phpmyadmin
