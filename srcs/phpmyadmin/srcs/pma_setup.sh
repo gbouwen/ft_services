d231257ec84bdda7dde26d0d315cb3688b4dc7b7
gbouwen <gijsbouwen@gmail.com>
2020-12-09 12:50:31 +0100

    added script to make sure phpmyamdin and nginx stay running

--- srcs/phpmyadmin/srcs/pma_setup.sh
+++ srcs/phpmyadmin/srcs/pma_setup.sh
@@ -2,3 +2,17 @@
 
 php-fpm7
 nginx -g "daemon off;"
+
+while true;
+do
+	ps -ef | grep php-fpm | grep master
+	if [ $? == 1 ]
+	then
+		break
+	fi
+	ps -ef | grep nginx | grep master
+	if [ $? == 1 ]
+	then
+		break
+	fi
+done
--- srcs/wordpress/srcs/wp_setup.sh
+++ srcs/wordpress/srcs/wp_setup.sh
@@ -9,3 +9,17 @@ sh tmp/install-wp.sh
 
 php-fpm7
 nginx -g "daemon off;"
+
+while true;
+do
+	ps -ef | grep php-fpm | grep master
+	if [ $? == 1 ]
+	then
+		break
+	fi
+	ps -ef | grep nginx | grep master
+	if [ $? == 1 ]
+	then
+		break
+	fi
+done
