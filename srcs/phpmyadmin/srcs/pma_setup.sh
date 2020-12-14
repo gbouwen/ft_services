204443905277ab6430626998e69a779e398fbda1
gbouwen <gijsbouwen@gmail.com>
2020-12-14 12:55:47 +0100

    fix component crash restart

--- srcs/phpmyadmin/srcs/pma_setup.sh
+++ srcs/phpmyadmin/srcs/pma_setup.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
 
 php-fpm7
-nginx -g "daemon off;"
+nginx
 
 while true;
 do
+	sleep 5
 	ps -ef | grep php-fpm | grep master
 	if [ $? == 1 ]
 	then
+		echo "$?"
+		echo "stopping..."
 		break
 	fi
 	ps -ef | grep nginx | grep master
 	if [ $? == 1 ]
 	then
+		echo "$?"
+		echo "stopping..."
 		break
 	fi
 done
