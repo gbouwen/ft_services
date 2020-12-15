9eebad3071151eaabfe27cd6b0e63ae1e2f6fca8
gbouwen <gijsbouwen@gmail.com>
2020-12-15 11:24:27 +0100

    fix ssh restart

--- srcs/nginx/srcs/nginx_setup.sh
+++ srcs/nginx/srcs/nginx_setup.sh
@@ -2,4 +2,23 @@
 
 etc/init.d/sshd start &
 
-nginx -g 'daemon off;'
+nginx
+
+while true;
+do
+	sleep 5
+	ps -ef | grep sshd | grep startups
+	if [ $? == 1 ]
+	then
+		echo $?
+		echo "stopping..."
+		break
+	fi
+	ps -ef | grep nginx | grep master
+	if [ $? == 1 ]
+	then
+		echo $?
+		echo "stopping..."
+		break
+	fi
+done
